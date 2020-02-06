5d16a769153765ad196849e1386320d5eb2931ee
[Brandon] <desselle.brandon2@gmail.com>
2020-02-06 05:00:50 -0600

    Create axiosWithAuth.js

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+
+const axiosWithAuth = type => {
+    const token = localStorage.getItem("token");
+    // https://lambda-mud-test.herokuapp.com/
+    // https://lambda-cs25-mud.herokuapp.com
+    const apiUrl = 'https://mud-test-ag.herokuapp.com';
+
+    let key
+    if (type == 'auth') {
+        key = "X-CSRFToken";
+    } else if (type == 'game') {
+        key = "Authorization"
+    };
+
+    return axios.create({
+        baseURL: apiUrl,
+        headers: {
+            "Content-Type": "application/json",
+            [key]: `Token ${token}`
+        }
+    });
+};
+
+export default axiosWithAuth;
